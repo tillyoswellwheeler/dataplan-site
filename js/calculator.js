5cf4b17a9d104b1060dfeb683c86b1e27c3f0792
Tilly Oswell-Wheeler <tilly.oswellwheeler@hotmail.com>
2018-11-21 11:50:34 +0000

    Commiting the addition of a calculator

---
+++ js/calculator.js
@@ -0,0 +1,17 @@
+// Chatterbox Calculator
+
+// Define variables
+
+var GB = 0;
+var mins = 0;
+
+var total = 0;
+
+// User(clicks) selects GB
+
+
+// User(clicks) selects mins
+// GB and mins values added together—monthly is the default and var total
+// listen for user clicks annually
+// listen for user clicks Monthly
+// Button BUY NOW appears after var total sum function done
