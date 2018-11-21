3938d4cc3b56a3f5d018fc3085dbf9d35fb041dc
Tilly Oswell-Wheeler <tilly.oswellwheeler@hotmail.com>
2018-11-21 12:04:20 +0000

    Commiting the addition of js for calculator

--- js/calculator.js
+++ js/calculator.js
@@ -2,16 +2,28 @@
 
 // Define variables
 
-var GB = 0;
+var gb = 0;
 var mins = 0;
 
 var total = 0;
 
 // User(clicks) selects GB
 
+$('GB').click(function(event) {
+  var gb = parseInt(this.getAttribute("value"));
+}
 
 // User(clicks) selects mins
+
+$('minutes').click(function(event) {
+  var mins = parseInt(this.getAttribute("value"));
+}
+
 // GB and mins values added together—monthly is the default and var total
+
+function options_added() {
+  var total = gb + mins;
+}
 // listen for user clicks annually
 // listen for user clicks Monthly
 // Button BUY NOW appears after var total sum function done
