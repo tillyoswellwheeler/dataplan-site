254853e8a87beb68b60fd385d16bf87f6c974cd2
Tilly Oswell-Wheeler <tilly.oswellwheeler@hotmail.com>
2018-11-22 15:02:09 +0000

    Creating the js for the family plan calculator

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+
+var wolf-tab
+var chatterbox-tab
+var family-tab
+
+$("#pills-wolf-tab").click(function(){
+    $("#wolf--card").css("background-color", "rgba(21, 127, 251, 0.4)");
+    $("#card-title-wolf").css("color", "white");
+    return wolf-tab
+});
+
+$("#pills-chatterbox-tab").click(function(){
+    $("#chatterbox--card").css("background-color", "rgba(21, 127, 251, 0.4)");
+    $("#card-title-chatterbox").css("color", "white");
+    return chatterbox-tab
+});
+
+$("#pills-family-tab").click(function(){
+    $("#family--card").css("background-color", "rgba(21, 127, 251, 0.4)");
+    $("#card-title-family").css("color", "white");
+    return family-tab
+});
+
+$("#pills-wolf-tab").click(function(){
+    return wolf-tab
+
+    if (condition1) {
+        block of code to be executed if condition1 is true
+    } else if (condition2) {
+        block of code to be executed if the condition1 is false and condition2 is true
+    } else {
+        block of code to be executed if the condition1 is false and condition2 is false
+    }
+
+  })
