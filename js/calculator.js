a32db84abd079085b0119997b4f246834cfb1483
Tilly Oswell-Wheeler <tilly.oswellwheeler@hotmail.com>
2018-11-23 10:17:32 +0000

    Working on the index card css and the script js for the highlight feature

--- js/calculator.js
+++ js/calculator.js
@@ -48,54 +48,54 @@ $('#chatterbox--calculate').click(function(event) {
 // var twenty-gb = ["30.00", "50.00", "70.00", "90.00", "110.00"];
 // var forty-gb = ["40.00", "68.00", "96.00", "124.00", "152.00"];
 
-var one-gb = 0;
-var ten-gb = 0;
-var twenty-gb = 0;
-var forty-gb = 0;
-
-var total = 0;
-var schedule = 0;
-
-$("#family-gb").show();
-$("#family-sims").hide();
-
-$('#family input[name="plan--price"]:checked').click(function(event) {
-  var chosen-gb = this.getAttribute('#family input[name="plan--price"]:checked');
-
-      if ( chosen-gb == one-gb ) {
-      $("#family-one-gb").show();
-      var chosen-sim = $('#family-one-gb input[id="plan--sim"]:checked').val();
-
-      return schedule;
-
-    } else if  ( chosen-gb == ten-gb ) {
-      $("#family-ten-gb").show();
-      var chosen-sim = $('#family-ten-gb input[id="plan--sim"]:checked').val();
-
-      return schedule;
-
-    } else if  ( chosen-gb == twenty-gb ) {
-      $("#family-twenty-gb").show();
-      var chosen-sim = $('#family-twenty-gb input[id="plan--sim"]:checked').val();
-
-      return schedule;
-
-    } else if  ( chosen-gb == forty-gb ) {
-      $("#family-forty-gb").show();
-      var chosen-sim = $('#family-forty-gb input[id="plan--sim"]:checked').val();
-
-      return schedule;
-
-    } else {
-      // Do nothing
-    }
-  })
-
-  function schedule() {
-    var schedule = $('#family input[name="plan--schedule"]:checked').val();
-    schedule = parseInt(schedule);
-    var total = chosen-sim * schedule;
-    console.log('chosen-gb ' + price + ' sched ' + schedule);
-
-    $('#family .plan--cost p').html('Total cost: £' + total);
-  }
+// var one-gb = 0;
+// var ten-gb = 0;
+// var twenty-gb = 0;
+// var forty-gb = 0;
+//
+// var total = 0;
+// var schedule = 0;
+//
+// $("#family-gb").show();
+// $("#family-sims").hide();
+//
+// $('#family input[name="plan--price"]:checked').click(function(event) {
+//   var chosen-gb = this.getAttribute('#family input[name="plan--price"]:checked');
+//
+//       if ( chosen-gb == one-gb ) {
+//       $("#family-one-gb").show();
+//       var chosen-sim = $('#family-one-gb input[id="plan--sim"]:checked').val();
+//
+//       return schedule;
+//
+//     } else if  ( chosen-gb == ten-gb ) {
+//       $("#family-ten-gb").show();
+//       var chosen-sim = $('#family-ten-gb input[id="plan--sim"]:checked').val();
+//
+//       return schedule;
+//
+//     } else if  ( chosen-gb == twenty-gb ) {
+//       $("#family-twenty-gb").show();
+//       var chosen-sim = $('#family-twenty-gb input[id="plan--sim"]:checked').val();
+//
+//       return schedule;
+//
+//     } else if  ( chosen-gb == forty-gb ) {
+//       $("#family-forty-gb").show();
+//       var chosen-sim = $('#family-forty-gb input[id="plan--sim"]:checked').val();
+//
+//       return schedule;
+//
+//     } else {
+//       // Do nothing
+//     }
+//   })
+//
+//   function schedule() {
+//     var schedule = $('#family input[name="plan--schedule"]:checked').val();
+//     schedule = parseInt(schedule);
+//     var total = chosen-sim * schedule;
+//     console.log('chosen-gb ' + price + ' sched ' + schedule);
+//
+//     $('#family .plan--cost p').html('Total cost: £' + total);
+//   }
--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,41 @@
 
-var wolf-tab
-var chatterbox-tab
-var family-tab
+var wolf-tab = 0;
+var chatterbox-tab = 0;
+var family-tab = 0;
 
 $("#pills-wolf-tab").click(function(){
     $("#wolf--card").css("background-color", "rgba(21, 127, 251, 0.4)");
     $("#card-title-wolf").css("color", "white");
-    return wolf-tab
+    // return wolf-tab;
 });
 
 $("#pills-chatterbox-tab").click(function(){
     $("#chatterbox--card").css("background-color", "rgba(21, 127, 251, 0.4)");
     $("#card-title-chatterbox").css("color", "white");
-    return chatterbox-tab
+    // return chatterbox-tab;
 });
 
 $("#pills-family-tab").click(function(){
     $("#family--card").css("background-color", "rgba(21, 127, 251, 0.4)");
     $("#card-title-family").css("color", "white");
-    return family-tab
+    // return family-tab;
 });
 
-$("#pills-wolf-tab").click(function(){
-    return wolf-tab
+$("button").click(function(){
+    $("input").trigger("select");
+});
 
-    if (condition1) {
-        block of code to be executed if condition1 is true
-    } else if (condition2) {
-        block of code to be executed if the condition1 is false and condition2 is true
-    } else {
-        block of code to be executed if the condition1 is false and condition2 is false
-    }
 
-  })
+
+// $("#pills-wolf-tab").click(function(){
+//     return wolf-tab;
+//
+//     if (condition1) {
+//         block of code to be executed if condition1 is true
+//     } else if (condition2) {
+//         block of code to be executed if the condition1 is false and condition2 is true
+//     } else {
+//         block of code to be executed if the condition1 is false and condition2 is false
+//     }
+//
+//   })
